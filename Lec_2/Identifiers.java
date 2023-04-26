aa0683e12290a80096b6f70d4b5eda99b9233d5e
Ayush-CB <130039413+Ayush-CB@users.noreply.github.com>
2023-04-26 23:00:25 +0530

    Datatypes and operators

---
+++ Lec_2/Identifiers.java
@@ -0,0 +1,19 @@
+package Lec_2;
+
+public class Identifiers {
+    public static void main(String[] args) {
+        //Point 1
+        //int h e=20;
+        //Point 2
+        //int 1h=15;
+        //Point 3
+        //int h2ee=9;
+        //Point 4
+        //int &a=5;
+        //Point 5
+        int a=5;
+        int A=5;
+        int _b=6;
+
+    }
+}
---
+++ Lec_2/Operators.java
@@ -0,0 +1,13 @@
+package Lec_2;
+
+public class Operators {
+    public static void main(String[] args) {
+        //ARITHMETIC
+        System.out.println(7/2);
+        //COMPARISON
+        System.out.println(7>=2);
+        //LOGICAl
+        System.out.println((7<2)&&(7>=2));
+        System.out.println((7<2)||(7>=2));
+    }
+}
