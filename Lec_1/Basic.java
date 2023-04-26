e49b4441c0b5375eed20ba81782a87a9d10a0152
Ayush-CB <130039413+Ayush-CB@users.noreply.github.com>
2023-04-26 22:59:35 +0530

    Basics

---
+++ Lec_1/Basic.java
@@ -0,0 +1,14 @@
+package Lec_1;
+
+import java.util.Scanner;
+
+public class Basic {
+    public static void main(String[] args) {
+//        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(n);
+    }
+}
