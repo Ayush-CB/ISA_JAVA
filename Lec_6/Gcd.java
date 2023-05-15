94f145fb9720f002fb18cfe1970a8c0956c1953f
Ayush-CB <130039413+Ayush-CB@users.noreply.github.com>
2023-05-15 23:08:00 +0530

    For loop Qps

---
+++ Lec_6/Gcd.java
@@ -0,0 +1,20 @@
+package Lec_6;
+
+import java.util.Scanner;
+
+public class Gcd {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        int divisor=a;
+        int dividend=b;
+        int remainder=-1;
+        while(remainder!=0){
+            remainder=dividend%divisor;
+            dividend=divisor;
+            divisor=remainder;
+        }
+        System.out.println(dividend);
+    }
+}
